147da9ea08326fe5a2cc559d589d317514eab72c
Aldoleon22 <leonaldo2606@gmail.com>
2025-02-11 10:01:40 +0300

    notif toast1

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -11,7 +11,7 @@ import { Label } from "@/components/ui/label"
 import { Alert, AlertTitle, AlertDescription } from "@/components/ui/alert"
 import { Loader } from "@/components/ui/loader"
 import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+
 
 function AuthPageContent() {
   const [isLoading, setIsLoading] = useState<boolean>(false)
@@ -27,9 +27,6 @@ function AuthPageContent() {
       {
         position: 'top-right',
         autoClose: 4000,
-        // Ajouter des classes de style ShadCN ici
-        className: 'bg-green-500 text-white rounded-lg shadow-lg p-4 text-sm font-medium',
-        bodyClassName: 'font-semibold',
       }
     );
   };
@@ -113,7 +110,7 @@ function AuthPageContent() {
 
          //nofication toastify//
          showToast();
-         
+
         setSuccessNotification("Inscription réussie. Un e-mail d'activation a été envoyé pour valider votre compte.")
         setSuccess("Inscription réussie. Un e-mail d'activation a été envoyé pour valider votre compte.")
         setTimeout(() => {
